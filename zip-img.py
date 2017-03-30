e9eb7324a0afe2335a7eb515f94ef6e3ab5f8bc3
Stepan Martynov <smartd88@gmail.com>
2017-03-30 10:22:34 +0300

    fin2

--- zip-img.py
+++ zip-img.py
@@ -2,30 +2,6 @@ import os
 import subprocess
 import glob
 
-# import os.path
-
-# def create_file_list(files, part_of_file):
-# 	files_list = []
-# 	for file in files:
-# 		with open(file) as f:
-# 			if part_of_file in f.read():
-# 				files_list.append(file)
-# 	return files_list
-
-
-# def main():
-# 	input_dirname = input('В какой папке будем искать?: ')
-# 	files = glob.glob(os.path.join(input_dirname, "*.sql"))
-
-# 	while True:
-# 		part_of_file = input('Введите часть файла, которую помните: ')
-# 		files = create_file_list(files, part_of_fileof_file)
-# 		print(files)
-# 		print(len(files))
-
-# if __name__ == '__main__':
-# 	main()
-
 def cp_imgs(imgs, dirname_source, dirname_copy):
 	for img in imgs:
 		# img_path_copy = dirname_copy + '/' + img
